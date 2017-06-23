f471ff21a8c4e442cb2cf52765165ad4c33c1f3f
Alex Carreira <aycarrei@gmail.com>
2017-06-23 00:32:01 -0700

    Introduce test mode to allow easy switch between sugarinfoAI and sugarinfoAITest Test work for webview + bootstrap

--- sugarbot/modules/reportUtils.js
+++ sugarbot/modules/reportUtils.js
@@ -5,29 +5,85 @@ exports.writeReportToS3 = function(date, userId, snapshot) {
     secretAccessKey: 'wWWu9XNsNzH6ydfbKKcQkp0drZcQKaSZRWYcNGHF',
     region: 'us-west-2',
   })
+
+  // Messenger Extensions script (required in html loaded in webview)
+  //
+  const msgrExtensionsScript = ' \
+    <script> \
+      (function(d, s, id) { \
+        var js, fjs = d.getElementsByTagName(s)[0]; \
+        if (d.getElementById(id)) { \
+          return; \
+        } \
+        js = d.createElement(s); \
+        js.id = id; \
+        js.src = "//connect.facebook.com/en_US/messenger.Extensions.js"; \
+        fjs.parentNode.insertBefore(js, fjs); \
+      } (document, \'script\', \'Messenger\')); \
+    </script>'
+
+  // Create HTML for the reports we wish to see:
+  // 1. (MVP) List of items for the day
+  // 2. Pie-chart showing amount consumed vs. goal / remaining
+  // 3. Progress on weight vs sugar Consumption
+  //
+
   const sugarIntake = snapshot.child('/sugarIntake/' + date).val()
-  let cleanDate = date.replace(/ /g, '_')
-  // AC TODO: use mustache here to template in our list
-  const msgrExtensionsScript =
-    '<script>(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";fjs.parentNode.insertBefore(js, fjs);}(document, \'script\', \'Messenger\'));</script>'
-  const reportHtml = '<!DOCTYPE html><html><head><title>Hello World!</title></head><body>'
-    + msgrExtensionsScript + '<h1>Hello World!</h1></body></html>'
+
+  const title = 'Sugar Info - ' + date
+  const hasData = snapshot.exists() &&
+                  snapshot.child('sugarIntake').exists() &&
+                  snapshot.child('sugarIntake/' + date).exists()
+
+  let sugarConsumptionReport = '<h1>Sugar Consumption Today</h1>'
+  if (!hasData) {
+    sugarConsumptionReport += '<p>You have not added any foods to your journal today.</p>'
+  } else {
+    sugarConsumptionReport += '<ol>'
+
+    const sugarConsumptionToday = snapshot.child('sugarIntake/' + date).val()
+    for (let key in sugarConsumptionToday) {
+      if (key === 'dailyTotal') {
+        continue
+      }
+      sugarConsumptionReport += '<li>' + sugarConsumptionToday[key].foodName + '</li>'
+    }
+
+    sugarConsumptionReport += '</ol>'
+    sugarConsumptionReport += '<p>Total Sugar ' + sugarConsumptionToday['dailyTotal'].sugar + ' grams</p>'
+  }
+
+  const reportHtml = ' \
+    <!DOCTYPE html> \
+    <html> \
+      <head> \
+        <title>' + title + '</title> \
+      </head> \
+      <body>' +
+        msgrExtensionsScript + ' \
+        ' + sugarConsumptionReport + ' \
+      </body> \
+    </html>'
+
+  const now = new Date(Date.now())
+  const datum = new Date(now.getFullYear(), now.getMonth(), 0, 0, 0, 0)
+  const offset = Date.now() - datum.getTime()
+
   const params = {
     Bucket: 'www.inphood.com',
-    Key: 'reports/' + userId + '/' + cleanDate + '.html',
+    Key: 'reports/' + userId + '/' + offset + '.html',
     // Key: 'reports/' + userId + '.html',
     Body: reportHtml,
     ContentType: 'text/html',
     ACL: 'public-read'
   }
-  console.log('BEFORE s3.upload(...).promise:')
+
   const s3promise = s3.upload(params).promise()
   return s3promise
   .then(info => {
-    console.log('AFTER s3.upload(...).promise:')
-    console.log('info: ' + info)
-    const dataUrl = 'https://www.inphood.com/reports/' + userId + '/' + cleanDate + '.html'
-    return "File written: " + dataUrl
+    // const dataUrl = 'https://' + params.Bucket + '/' + params.Key
+    const dataUrl = 'https://' + params.Bucket + '/' + 'reports/test/reportBootstrap.html'
+    return dataUrl
   })
   .catch(error => console.log(error));
 }
