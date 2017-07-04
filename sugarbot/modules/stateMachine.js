d9f698a4044cf51480dcf46c0101b08959c45a0d
inphood <prabhaav.bhardwaj@gmail.com>
2017-07-03 20:54:32 -0700

    local stuff

--- sugarbot/modules/stateMachine.js
+++ sugarbot/modules/stateMachine.js
@@ -93,57 +93,6 @@ exports.bot = function(request, messageText, userId) {
             ]
           }
         }
-        case 'debug_msgr_extensions': {
-          const wvMsg = {
-            uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAJhTtF5K30BABsLODz0w5Af5hvd1SN9TZCU0E9OapZCKuZAOMugO2bNDao8JDe8E3cPQrJGLWWfL0sMxsq4MSTcZBbgGEjqa68ggSZCmZAFhGsFPFkWGUlYwAZB2ZCOrPPgdxS612ck5Rv8SrHydJihKQGsPLQSc1yYtBkncIpbOgZDZD',
-            json: true,
-            method: 'POST',
-            body: {
-              'recipient':{
-                'id':userId
-              },
-              'message':{
-                'attachment':{
-                  'type':'template',
-                  "payload":{
-                    "template_type":"generic",
-                    "elements":[
-                       {
-                        "title":"sugarinfoAI Daily Report",
-                        "image_url":"https://d1q0ddz2y0icfw.cloudfront.net/chatbotimages/arrows.jpg",
-                        "subtitle":userId + "'s sugar consumption through " + date,
-                        "default_action": {
-                          "url": 'https://s3-us-west-1.amazonaws.com/www.inphood.com/reports/test/testWebExtensions.html',
-                          "type": "web_url",
-                          "messenger_extensions": true,
-                          "webview_height_ratio": "tall",
-                          "fallback_url": "https://www.inphood.com/"
-                        },
-                        "buttons":[
-                          {
-                            "url":'https://s3-us-west-1.amazonaws.com/www.inphood.com/reports/test/testWebExtensions.html',
-                            "type":"web_url",
-                            "title":"View Report",
-                            "webview_height_ratio": "tall"
-                          },
-                          {
-                            "type":"element_share"
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                }
-              }
-            },
-            resolveWithFullResponse: true,
-            headers: {
-              'Content-Type': "application/json"
-            }
-          }
-
-          return requestPromise(wvMsg)
-        }
         case 'other options': {
           return utils.otherOptions(false)
         }
